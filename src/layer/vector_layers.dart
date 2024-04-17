8fe50354d2922f013ab76f741347505435207929
pavlogrinchenkoo <pavlogrinchenkoo@gmail.com>
2024-04-17 10:34:05 +0300

    init

--- src/layer/vector_layers.dart
+++ src/layer/vector_layers.dart
@@ -6,7 +6,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:syncfusion_flutter_core/theme.dart';
 
 import '../../maps.dart';
